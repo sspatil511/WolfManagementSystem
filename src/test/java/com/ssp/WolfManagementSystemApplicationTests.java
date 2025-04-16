beb6e9145acf55db7e41d4692afae5ae93a4413f
Soham Patil <sspatil6@ncsu.edu>
2025-04-16 14:15:35 -0400

    Initial commit

---
+++ src/test/java/com/ssp/WolfManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
