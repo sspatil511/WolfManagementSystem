e987d70f15fd22889e8a8842b07161fb04a83d2f
Soham Patil <sspatil6@ncsu.edu>
2025-12-01 09:54:56 -0500

    Update Chat and Message

--- src/main/java/com/ssp/model/Chat.java
+++ src/main/java/com/ssp/model/Chat.java
@@ -3,10 +3,15 @@ package com.ssp.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,5 +33,11 @@ public class Chat {
     @OneToOne
     private Project project;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> messages = new ArrayList<>();
+
+    @ManyToMany
+    private List<User> users = new ArrayList<>();
+
 }
--- src/main/java/com/ssp/model/Message.java
+++ src/main/java/com/ssp/model/Message.java
@@ -1,5 +1,37 @@
 package com.ssp.model;
 
-public class Message {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Chat chat;
+
+    @ManyToOne
+    private User sender;
+    
 }
