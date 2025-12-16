42e9e73749185f0e6b0562cf5fa5b6dd3000f075
Soham Patil <sspatil6@ncsu.edu>
2025-12-16 11:29:09 -0500

    Update invitation service

---
+++ src/main/java/com/ssp/repository/InviteRequest.java
@@ -0,0 +1,16 @@
+package com.ssp.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class InviteRequest {
+
+    private Long projectId;
+    private String email;
+
+}
