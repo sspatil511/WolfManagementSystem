046f5e3c50783750fe29b1deab4c4972ce967252
Soham Patil <sspatil6@ncsu.edu>
2026-01-13 16:28:02 -0500

    Implement message controller

---
+++ src/main/java/com/ssp/controller/MessageController.java
@@ -0,0 +1,59 @@
+package com.ssp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssp.model.Chat;
+import com.ssp.model.Message;
+import com.ssp.model.User;
+import com.ssp.request.CreateMessageRequest;
+import com.ssp.service.MessageService;
+import com.ssp.service.ProjectService;
+import com.ssp.service.UserService;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("/send")
+    public ResponseEntity<Message> sendMessage(@RequestBody CreateMessageRequest request) throws Exception{
+
+        //User user = userService.findUserById(request.getSenderId());
+
+        Chat chat = projectService.getProjectById(request.getProjectId()).getChat();
+
+        if(chat == null) {
+
+            throw new Exception("Chats not found");
+        }
+
+        Message sentMessage = messageService.sendMessage(request.getSenderId(), request.getProjectId(), request.getContent());
+
+        return ResponseEntity.ok(sentMessage);
+    }
+
+    @GetMapping("/chat/{projectId}")
+    public ResponseEntity<List<Message>> getMessagesByChatId(@PathVariable Long projectId) throws Exception {
+
+        List<Message> messages = messageService.getMessagesByProjectId(projectId);
+
+        return ResponseEntity.ok(messages);
+    }
+}
---
+++ src/main/java/com/ssp/request/CreateMessageRequest.java
@@ -0,0 +1,13 @@
+package com.ssp.request;
+
+import lombok.Data;
+
+@Data
+public class CreateMessageRequest {
+
+    private Long senderId;
+
+    private String content;
+
+    private Long projectId;
+}
