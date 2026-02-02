f2f442b10cf181892d6304797b9581d906e99d2f
Soham Patil <sspatil6@ncsu.edu>
2026-02-02 15:35:12 -0500

    Fix services

--- src/main/java/com/ssp/service/MessageServiceImpl.java
+++ src/main/java/com/ssp/service/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssp.model.Chat;
 import com.ssp.model.Message;
@@ -11,6 +12,7 @@ import com.ssp.model.User;
 import com.ssp.repository.MessageRepository;
 import com.ssp.repository.UserRepository;
 
+@Service
 public class MessageServiceImpl implements MessageService{
 
     @Autowired
--- src/test/java/com/ssp/WolfManagementSystemApplicationTests.java
+++ src/test/java/com/ssp/WolfManagementSystemApplicationTests.java
@@ -2,9 +2,20 @@ package com.ssp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.mail.username=test",
+        "spring.mail.password=test",
+        "spring.config.import="
+    }
+)
 class WolfManagementSystemApplicationTests {
+
+	@MockitoBean
+    private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
