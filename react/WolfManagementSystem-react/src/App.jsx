30d15380ad5c37ab71a6970006a3f70279e88dfd
Soham Patil <sspatil6@ncsu.edu>
2026-02-20 11:33:37 -0500

    Setup frontend

---
+++ react/WolfManagementSystem-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import { Button } from "@/components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <Button variant="default" size="default">Click me</Button>
+  )
+}
+
+export default App
